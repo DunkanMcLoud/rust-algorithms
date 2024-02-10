9bd03d4d416d38b50a44148cdf4043924d6e28b7
Andrei Dunai <samurai.satoshisan@gmail.com>
2024-02-10 13:24:53 +0300

    borrowing issues

--- graphs/kosaraju/src/lib.rs
+++ graphs/kosaraju/src/lib.rs
@@ -1,24 +1,29 @@
-use std::usize;
+use std::{
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
+    rc::Rc,
+    usize,
+};
 
 #[derive(Debug, Default)]
-struct Graph {
-    nodes: Vec<Node>,
+struct DirectedGraph {
+    nodes: Vec<RefCell<Node>>,
 }
 
-impl Graph {
-    fn add_connection(&mut self, from: usize, to: usize) {
+impl DirectedGraph {
+    fn add_edge(&mut self, from: usize, to: usize) {
         if self.nodes.len() < from {
-            self.nodes.resize_with(from, Node::default);
+            self.nodes.resize_with(from, Node::new);
         }
-        self.nodes[from - 1].connect_to(to)
+        self.nodes[from - 1].borrow_mut().connect_to(to)
     }
 
-    fn reversed(&self) -> Graph {
-        let mut rev = Graph::default();
+    fn reversed(&self) -> DirectedGraph {
+        let mut rev = DirectedGraph::default();
 
         for (index, node) in self.nodes.iter().enumerate() {
-            for adj_node_id in node.adjacent_node_ids {
-                rev.add_connection(adj_node_id, index + 1)
+            for adj_node_id in node.borrow().adjacent_node_ids.iter() {
+                rev.add_edge(*adj_node_id, index + 1)
             }
         }
         rev
@@ -27,36 +32,82 @@ impl Graph {
 
 #[derive(Debug, Default)]
 struct Node {
-    is_visited: bool,
     // ordering starts from 1
     adjacent_node_ids: Vec<usize>,
+    meta: TraverseMeta,
 }
 
 impl Node {
     fn connect_to(&mut self, to: usize) {
         self.adjacent_node_ids.push(to);
     }
+
+    fn is_visited(&self) -> bool {
+        self.meta.is_visited
+    }
+
+    fn mark_as_visited(&mut self) {
+        self.meta.mark_as_visited()
+    }
+
+    fn new() -> RefCell<Node> {
+        RefCell::new(Node::default())
+    }
 }
 
-impl From<Vec<Vec<usize>>> for Graph {
+impl From<Vec<Vec<usize>>> for DirectedGraph {
     fn from(src: Vec<Vec<usize>>) -> Self {
-        let mut graph = Graph::default();
+        let mut graph = DirectedGraph::default();
         src.into_iter()
-            .for_each(|tuple| graph.add_connection(tuple[0], tuple[1]));
+            .for_each(|tuple| graph.add_edge(tuple[0], tuple[1]));
         graph
     }
 }
 
-fn top_5_scc_sizes(graph: &Graph) -> [usize; 5] {
-    let rev: Graph = graph.reversed();
+#[derive(Default, Debug)]
+struct TraverseMeta {
+    topo_order: usize,
+    is_visited: bool,
+}
+
+impl TraverseMeta {
+    fn mark_as_visited(&mut self) {
+        self.is_visited = true
+    }
+}
+
+fn topo_sort(graph: Rc<DirectedGraph>) {
+    let mut cur_label = graph.nodes.len();
+    for (index, node) in graph.nodes.iter().enumerate() {
+        if !node.borrow().is_visited() {
+            dfs_topo(Rc::clone(&graph), index, &mut cur_label)
+        }
+    }
+}
+
+fn dfs_topo(graph: Rc<DirectedGraph>, node_id: usize, topo_label: &mut usize) {
+    let selected_node = graph.nodes.get(node_id).unwrap();
+    selected_node.borrow_mut().mark_as_visited();
+    for node_id in &selected_node.borrow().adjacent_node_ids {
+        if !graph.nodes[node_id - 1].borrow().is_visited() {
+            dfs_topo(Rc::clone(&graph), node_id - 1, topo_label)
+        }
+    }
+    selected_node.borrow_mut().meta.topo_order = *topo_label;
+    *topo_label -= 1;
+}
+
+fn top_5_scc_sizes(graph: &DirectedGraph) -> [usize; 5] {
+    let rev: DirectedGraph = graph.reversed();
+    [0, 0, 0, 0, 0]
 }
 
 #[cfg(test)]
 mod tests {
 
-    use std::fs::File;
+    use std::{fs::File, rc::Rc};
 
-    use crate::{top_5_scc_sizes, Graph};
+    use crate::{top_5_scc_sizes, topo_sort, DirectedGraph};
 
     #[test]
     fn test_one() {
@@ -64,9 +115,10 @@ mod tests {
         let matrix = utils::read_matrix(m1);
 
         dbg!(&matrix);
-        let graph = Graph::from(matrix);
+        let graph = Rc::new(DirectedGraph::from(matrix));
 
-        assert_eq!(top_5_scc_sizes(&graph), [3, 3, 3, 0, 0]);
-        dbg!(graph);
+        // assert_eq!(top_5_scc_sizes(&graph), [3, 3, 3, 0, 0]);
+        topo_sort(Rc::clone(&graph));
+        dbg!(&graph);
     }
 }
